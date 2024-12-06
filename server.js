50555e499d55ea56a11070c2f6b2ff455c539148
giuse <giuseppedefilippo03@gmail.com>
2024-12-06 15:57:01 +0100

    aggiunto il file env e ho svolto lesercizio

--- server.js
+++ server.js
@@ -3,19 +3,17 @@
 
 const express = require("express")
 const app = express()
-const PORT = 3000
+const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(express.static("public"));
 app.use("/bacheca", require("./routers/post.js"));
-app.get("/", (req, res) => {
-    res.send("server del mio blog")
-})
 app.use("/utenti", require("./routers/listaUtenti.js"))
 app.use("/macchine", require("./routers/macchine.js"))
+app.use(require("./middleware/postErroreMIddleware.js"));
+app.use(require("./middleware/gestioneRotteMIddleware.js"));
 
-
-app.listen(PORT, () => {
-    console.log(`Server in esecuzione su http://localhost:${PORT}`);
-});
+app.listen(process.env.port, () => {
+    console.log(`Server in esecuzione su http://localhost:${process.env.port}`);
+}); 
