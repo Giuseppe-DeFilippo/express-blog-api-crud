9103c2edefe5dc4e336d03324a819de459fc9406
giuse <giuseppedefilippo03@gmail.com>
2025-01-07 15:06:32 +0100

    aggiornamento

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 
 // - Create router, controller e model per un'altra risorsa a vostra discrezione (es. commenti, utenti, ...)
 
-const express = require("express")
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const app = express();
 const port = process.env.PORT || 3000;
-const bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+
+//middleware
+app.use(bodyParser.json());
 app.use(express.static("public"));
-app.use("/bacheca", require("./routers/post.js"));
-app.use("/utenti", require("./routers/listaUtenti.js"))
-app.use("/macchine", require("./routers/macchine.js"))
-app.use(require("./middleware/postErroreMIddleware.js"));
-app.use(require("./middleware/gestioneRotteMIddleware.js"));
+app.use(cors());
+
+app.use("/api/post", postRouter);
 
 app.listen(process.env.port, () => {
     console.log(`Server in esecuzione su http://localhost:${process.env.port}`);
